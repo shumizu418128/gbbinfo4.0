33d0ba75887f70dc55233ad9aebe2c37cf1e0e0e
Haruki Shimizu <shumizu418128@gmail.com>
2026-05-03 22:02:02 +0900

    feat: @inlang/paraglide-jsを追加し設定を更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }),tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
