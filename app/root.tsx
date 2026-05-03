31ff5750333ca9e0ac9337027fc60c7559413b46
Haruki Shimizu <shumizu418128@gmail.com>
2026-05-03 13:02:08 +0900

    feat: HTMLの言語設定を日本語に変更

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export const links: Route.LinksFunction = () => [
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
