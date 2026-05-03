39b792693477b7a3af15628018dfa1c9e4f6d697
Haruki Shimizu <shumizu418128@gmail.com>
2026-05-03 10:32:55 +0900

    feat: ルート設定にインデックスを追加

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,6 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    route("/2026/top", "routes/2026/top.tsx")
+  index("routes/_index.tsx"),
+  route("/2026/top", "routes/2026/top.tsx"),
 ] satisfies RouteConfig;
---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "react-router";
+
+export function loader() {
+  return redirect("/2026/top");
+}
+
+export default function Index() {
+  return null;
+}
