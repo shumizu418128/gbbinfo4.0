7215a46b6c13b8f30cb94721a7b9e03874e4ca42
Haruki Shimizu <shumizu418128@gmail.com>
2026-06-06 14:59:01 +0900

    chore: react-router-server.mjsを削除

--- .netlify/v1/functions/react-router-server.mjs
+++
@@ -1,9 +0,0 @@
-export { default } from "../../../build/server/server.js";
-
-export const config = {
-  name: "React Router server handler",
-  generator: "@netlify/vite-plugin-react-router@3.1.1",
-  path: "/*",
-  excludedPath: ["/.netlify/*"],
-  preferStatic: true,
-};
